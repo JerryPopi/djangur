84cf05ab0b2ed4081c9228db386ba50331f19453
teoet6 <teoet6@protonmail.com>
2021-10-11 01:04:06 +0300

    ytdl search

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"strings"
 	"os"
 	"os/signal"
+	"os/exec"
 	"syscall"
 	"flag"
+	"encoding/json"
+
 	"github.com/bwmarrin/discordgo"
 )
+
+type Song struct {
+	url string
+}
 
 func chk(err error){
 	if err != nil {
@@ -16,14 +23,43 @@ func chk(err error){
 	}
 }
 
+func songSearch(query string) (song Song) {
+	cmd := exec.Command("youtube-dl", "-j", query)
+	stdout, err := cmd.Output()
+	chk(err)
+
+	var video map[string]interface{}
+	json.Unmarshal(stdout, &video)
+
+	if video["formats"] != nil {
+		formats := video["formats"].([]interface{})
+		format  := formats[0].(map[string]interface{})
+		song.url = format["url"].(string)
+	} else if video["url"] != nil {
+		song.url = video["url"].(string)
+	}
+
+	return song
+}
+
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if !strings.HasPrefix(m.Content, "?") {
 		return
 	}
 	m.Content = m.Content[1:]
+	split := strings.SplitN(m.Content, " ", 2)
+	cmd  := split[0]
+	arg := ""
+	if len(split) == 2 {
+		arg = split[1]
+	}
 
-	if m.Content == "ping" {
+	switch cmd {
+	case "ping":
 		s.ChannelMessageSend(m.ChannelID, "pong")
+	case "play":
+		song := songSearch(arg)
+		fmt.Println(song.url)
 	}
 }
 
