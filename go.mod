45a2d0d0fe7e0f23c70391adbb41cb92c8e118b3
JerryPopi <45138831+JerryPopi@users.noreply.github.com>
2021-10-18 12:14:15 +0300

    puska zvuk

--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module github.com/JerryPopi/djangur
 
 go 1.17
 
-require github.com/bwmarrin/discordgo v0.23.2
+require (
+	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e
+	github.com/bwmarrin/discordgo v0.23.2
+)
 
 require (
 	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/jonas747/dca v0.0.0-20210930103944-155f5e5f0cc7
 	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
 )
+
+require github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"os"
-	"os/signal"
-	"os/exec"
-	"syscall"
-	"flag"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
+	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
+	"github.com/jonas747/dca"
+)
+
+const (
+	channels   int = 2     // 1 for mono, 2 for stereo
+	frameRate  int = 48000 // audio sampling rate
+	frameSize  int = 960   // uint16 size of each audio frame 960/48KHz = 20ms
+	bufferSize int = 1024  // max size of opus data 1K
 )
 
 type Song struct {
+	title string
 	url string
+	id string
+	duration string
+}
+
+type VoiceInstance struct {
+	voice      *discordgo.VoiceConnection
+	session    *discordgo.Session
+	encoder    *dca.EncodeSession
+	stream     *dca.StreamingSession
+	run        *exec.Cmd
+	queueMutex sync.Mutex
+	audioMutex sync.Mutex
+	nowPlaying Song
+	queue      []Song
+	recv       []int16
+	guildID    string
+	channelID  string
+	speaking   bool
+	pause      bool
+	stop       bool
+	skip       bool
+	radioFlag  bool
 }
 
 func chk(err error){
@@ -27,6 +60,7 @@ func songSearch(query string) (song Song) {
 	if !strings.HasPrefix(query, "https:") {
 		query = "ytsearch:" + query
 	}
+	println("Downloading video...")
 	cmd := exec.Command("youtube-dl", "-j", query)
 	stdout, err := cmd.Output()
 	chk(err)
@@ -37,12 +71,29 @@ func songSearch(query string) (song Song) {
 	if video["formats"] != nil {
 		formats := video["formats"].([]interface{})
 		format  := formats[0].(map[string]interface{})
+		fmt.Printf("%+v\n", formats)
 		song.url = format["url"].(string)
+		// song.duration = format["duration"].(string)
+		// song.title = format["title"].(string)
+		// song.id = format["id"].(string)
 	} else if video["url"] != nil {
 		song.url = video["url"].(string)
 	}
-	
+	println("Done downloading video!")
 	return song
+}
+
+func getUserVoiceChannel(s *discordgo.Session, m *discordgo.MessageCreate) (string){
+	guild, err := s.State.Guild(m.GuildID)
+	chk(err)
+
+	for _, vs := range guild.VoiceStates {
+		if m.Author.ID == vs.UserID {
+			return vs.ChannelID
+		}
+	}
+
+	return ""
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -61,8 +112,16 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	case "ping":
 		s.ChannelMessageSend(m.ChannelID, "pong")
 	case "play":
+		vc := getUserVoiceChannel(s, m)
+		if vc == "" {
+			s.ChannelMessageSend(m.ChannelID, "User not in voice channel!")
+			return
+		}
+		dgv, err := s.ChannelVoiceJoin(m.GuildID, vc, false, true)
+		chk(err)
 		song := songSearch(arg)
-		fmt.Println(song.url)
+		dgvoice.PlayAudioFile(dgv, song.url, make(chan bool))
+		fmt.Println(song)
 	}
 }
 
